b7a6115959f523eb189a84fc8f7fae0c1c42ff52
jace <nic.chiron@gmail.com>
2023-05-15 14:01:05 +0200

    validate seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+Event.destroy_all
+Attendance.destroy_all
+
+
+# Création des utilisateurs
+10.times do
+  User.create(
+    email: Faker::Internet.email(domain: "yopmail.com"),
+    encrypted_password: Faker::Internet.password,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    description: Faker::Lorem.paragraph
+  )
+end
+
+puts 'Utilisateurs créés avec succès.'
+
+# Création des événements
+3.times do
+  Event.create(
+    start_date: Faker::Time.between_dates(from: Date.today, to: Date.today + 30),
+    title: Faker::Lorem.sentence,
+    location: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph,
+    price: Faker::Number.between(from: 1, to: 1000),
+    duration: rand(1..100)*5,
+    administrator_id: User.all.sample.id
+  )
+end
+
+puts 'Événements créés avec succès.'
+
+# Création des participations
+5.times do
+  Attendance.create(stripe_customer_id: Faker::Alphanumeric.alphanumeric(number: 10), attendee_id: User.all.sample.id, event_id: Event.all.sample.id)
+end
+
+puts 'Participations créées avec succès.'
