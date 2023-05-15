0480935cabeae4cfe5b4af82b4218614a601170c
jace <nic.chiron@gmail.com>
2023-05-15 16:14:05 +0200

    correction v4 attendance mail

--- app/mailers/attendance_mailer.rb
+++ app/mailers/attendance_mailer.rb
@@ -7,7 +7,7 @@ class AttendanceMailer < ApplicationMailer
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
     @url = "http://monsite.fr/login"
      # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @user.email, subject: "Vous faites bien parti de l'évènement !")
+    mail(to: @attendee.email, subject: "Vous faites bien parti de l'évènement !")
   end
 
 end
