5996fbe9385d8c38f175c07beaf164f349697438
jace <nic.chiron@gmail.com>
2023-05-17 20:05:52 +0200

    destroy event

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   has_many :attendances, foreign_key: 'attendee_id', class_name: "Attendance", dependent: :destroy
   has_many :events, through: :attendances
-  has_many :events, foreign_key: 'administrator_id', class_name: "Event"
+  has_many :events, foreign_key: 'administrator_id', class_name: "Event", dependent: :destroy
 
   validates :description, presence:true
   validates :first_name, presence:true
